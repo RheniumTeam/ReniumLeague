0e93855e072b1acb17a757127306a253e0c1bd96
plamenvt <plamenvt@gmail.com>
2015-10-20 12:41:18 +0300

    xml generator added

--- application/ReniumLeague/ReniumLeage.UI/StartUp.cs
+++ application/ReniumLeague/ReniumLeage.UI/StartUp.cs
@@ -11,6 +11,26 @@
             ZipExtractor zipReader = new ZipExtractor("../../../../../Matches - Results.zip");
 
             zipReader.Extract("../../../../../");
+
+
+            MongoRepository mongoContext = new MongoRepository();
+
+            List<ReniumLeague.Entity.Mongo.Models.Team> allPlaces = mongoContext.GetAllTeams().ToList();
+
+            var xmlSerializer = new ReniumLeague.Entity.Serializer.XmlSerialzer();
+
+            var xmlString = xmlSerializer.Serialize < ReniumLeague.Entity.Mongo.Models.Team >> (allTeams);
+
+            var xmlFilePath = "../../teams.xml";
+
+            using (var str = new StreamWriter(xmlFilePath))
+            {
+                str.Write(xmlString);
+            }
+
+            var objectsFromXml = xmlSerializer.ParseXml < ReniumLeague.Entity.Mongo.Models.Team >> (xmlFilePath);
+
+            Console.WriteLine();
         }
     }
 }
